d08217dd471624b443a694dbce42bf0262ce3eb4
Truls Persson <tp222ff@student.lnu.se>
2014-09-06 16:22:45 +0200

    Visar ett exempel vi gick igenom på lektions tid
    {0.f1} för att avrunda till en decimal
    (double) för att två int = double och spara på bytes

---
+++ Test A for Github/Tester och eget skit/Alpha/ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int points;
+            int students;
+            double average;
+
+
+            Console.Write("Please enter the total score: ");
+            points = int.Parse(Console.ReadLine());
+
+            Console.Write("Please enter the total number of students: ");
+            students = int.Parse(Console.ReadLine());
+
+            average = (double)points / students;
+
+            Console.WriteLine("The average score of the class is {0:f1}.", average);
+
+            Console.ReadKey();
+        }
+    }
+}
