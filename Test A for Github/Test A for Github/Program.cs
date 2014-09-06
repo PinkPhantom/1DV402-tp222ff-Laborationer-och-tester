365a2a9384d64805b56f52037e0980a9dd4bf31f
Truls Persson <tp222ff@student.lnu.se>
2014-09-06 16:11:44 +0200

    Har skrivit en heltals adderare

--- Test A for Github/Test A for Github/Program.cs
+++ Test A for Github/Test A for Github/Program.cs
@@ -10,6 +10,27 @@ namespace Test_A_for_Github
     {
         static void Main(string[] args)
         {
+
+            int domo;
+            int arigato;
+            int roboto;
+
+            string mr;
+
+            Console.Write("Skriv det första heltalet : ");
+            mr = Console.ReadLine();
+            domo = int.Parse(mr);
+
+            Console.Write("Skriv det andra heltalet : ");
+            mr = Console.ReadLine();
+            arigato = int.Parse(mr);
+
+            roboto = domo + arigato;
+
+            Console.Write("Svaret är : {0}", roboto);
+
+            Console.ReadKey();
+
         }
     }
 }
