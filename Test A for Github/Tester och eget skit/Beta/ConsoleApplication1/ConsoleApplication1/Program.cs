00c939f3093952b8b47cfada3df05f40664e6dd7
Truls Persson <tp222ff@student.lnu.se>
2014-09-06 20:33:12 +0200

    Lotteri övningen
    Lärde mig lite hur "for" funkar.

---
+++ Test A for Github/Tester och eget skit/Beta/ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int vinst = 0;
+            // Lotter bestämmer hur högt den kan räkna till, lottpris bestämmer vad den ska hoppa mellan varje räkning
+
+            int lotter;
+            int lottpris;
+            Console.Write("Vad är den högsta summan?: ");
+            lotter = int.Parse(Console.ReadLine());
+
+            Console.Write("Vad är mellanskillnaden mellan varje lott?: ");
+            lottpris = int.Parse(Console.ReadLine());
+            for (int i = 0; i <=(lotter); i += (lottpris))
+
+                vinst += i;
+
+            Console.WriteLine(vinst);
+
+            Console.ReadKey();
+
+        }
+    }
+}
