e9e31c7fed398fa627bd63806482ae86fa9800ea
Truls Persson <tp222ff@student.lnu.se>
2014-09-06 21:22:15 +0200

    Har gjort Produkten av heltal
    Har lärt mig mer of "for" och att long är en variabel  för högre tal

---
+++ Test A for Github/Tester och eget skit/Charlie/Produkten av heltal/Produkten av heltal/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Produkten_av_heltal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           int max;
+           int hopp;
+           long product = 1;
+
+
+            // int max ökar hur högt det gångar med sig själ int hopp bestämmer var den börjar mellan max och hopp
+
+
+           Console.Write("Please enter the maximum multiplier: ");
+          max = int.Parse(Console.ReadLine());
+
+          Console.Write("Please enter the number to begin the multiplication: ");
+          hopp = int.Parse(Console.ReadLine());
+
+
+
+           for (int i = hopp; i <= max; i++)
+               //Product* tar emot loopen alla gånger fram tills den avbryter sig (altså när max antalet har nåtts)
+               product *= i;
+
+
+
+
+
+
+
+             Console.WriteLine("The product of all the numbers from {0} to {1} is {2}",hopp ,max , product);
+             Console.ReadKey();
+
+
+
+        }
+    }
+}
