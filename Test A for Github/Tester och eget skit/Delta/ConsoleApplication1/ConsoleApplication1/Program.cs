a4d43d5a54f91a7cfe32996060b5e06a61c46c11
Truls Persson <tp222ff@student.lnu.se>
2014-09-08 11:07:58 +0200

    Föreläsning 2014-09-08

---
+++ Test A for Github/Tester och eget skit/Delta/ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int value;
+
+
+            value = int.Parse(Console.ReadLine());
+
+            if (value < 10)
+            {
+
+                Console.WriteLine("Värdet {0} är mindre än 10", value);
+            }
+            
+                else if (value == 10)
+            {
+                Console.WriteLine("Värdet {0} är lika med 10", value);
+            }                
+
+            else
+
+             Console.WriteLine("Värdet {0} är större än 10", value);
+
+
+            Console.ReadKey();
+
+
+
+        }
+    }
+}
