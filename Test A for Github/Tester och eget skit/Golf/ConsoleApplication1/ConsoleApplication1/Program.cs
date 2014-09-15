29648ea243352255b8c5111fb715237ed036d5c2
Truls Persson <tp222ff@student.lnu.se>
2014-09-15 18:07:35 +0200

    Färdig med en basic labb 1

--- Test A for Github/Tester och eget skit/Golf/ConsoleApplication1/ConsoleApplication1/Program.cs
+++ Test A for Github/Tester och eget skit/Golf/ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -11,12 +11,10 @@ namespace ConsoleApplication1
         static void Main(string[] args)
         {
             //Variabler
-            double attbetala;
+            
             int betalamed;
-            int tillbaka;
-            double avrundning;
+            int tillbaka;            
             int attbetala2;
-            double efteravrudning;
 
             int femhundra;
             int hundra;
@@ -26,31 +24,54 @@ namespace ConsoleApplication1
             int fem;
             int enkrona;
 
-            Console.Write("Ange total summa    : ");
-            attbetala = double.Parse(Console.ReadLine());
-            attbetala2 = (int)attbetala;
-            avrundning = (double)attbetala - attbetala2;
-            efteravrudning = attbetala - avrundning;
-
-           Console.Write("Ange erhållet belopp: ");
-           betalamed = int.Parse(Console.ReadLine());
-           tillbaka = betalamed - (int)efteravrudning;
-
-            Console.WriteLine("Kvitto");
-            Console.WriteLine("--------------------------");
-
-            Console.WriteLine("Totalt : {0:f2}kr", attbetala);
-            Console.WriteLine("Öresavrunding : {0:f2}kr", avrundning);
-            Console.WriteLine("Att betala : {0}kr", efteravrudning);
-            Console.WriteLine("Kontant : {0}kr", betalamed);
-            Console.WriteLine("Tillbaka : {0}kr", tillbaka);
+            double efteravrudning;
+            double attbetala;
+            double avrundning;
+            
+            
 
 
+
+            ReadPositiveDouble(out betalamed, out attbetala2, out attbetala, out avrundning);
+
+
+            efteravrudning = ReadUint(attbetala, avrundning);
+
+
+
+
+
+            tillbaka = ReadConsole(betalamed, efteravrudning, attbetala, avrundning);
+
+            tillbaka = SplitIntodenominations(tillbaka, out femhundra, out hundra, out femtio, out tjugo, out tio, out fem, out enkrona);
+
+            
+
+
+
+            Console.ReadKey();
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+
+        private static int SplitIntodenominations(int tillbaka, out int femhundra, out int hundra, out int femtio, out int tjugo, out int tio, out int fem, out int enkrona)
+        {
             femhundra = tillbaka / 500;
             tillbaka %= 500;
 
             Console.WriteLine("500-lappar  : {0}st ", femhundra);
-            
+
             hundra = tillbaka / 100;
             tillbaka %= 100;
 
@@ -80,25 +101,55 @@ namespace ConsoleApplication1
             tillbaka %= 1;
 
             Console.WriteLine("1-kronor  : {0}st ", enkrona);
+            return tillbaka;
+        }
 
-            
+        private static int ReadConsole(int betalamed, double efteravrudning, double attbetala, double avrundning)
+        {
+            int tillbaka;
+            tillbaka = betalamed - (int)efteravrudning;
+
+            Console.WriteLine("Kvitto");
+            Console.WriteLine("--------------------------");
+            Console.WriteLine("Totalt          : {0:f2}kr", attbetala);
+            Console.WriteLine("Öresavrunding   : {0:f2}kr", avrundning);
+            Console.WriteLine("Att betala      : {0}kr", efteravrudning);
+            Console.WriteLine("Kontant         : {0}kr", betalamed);
+            Console.WriteLine("Tillbaka        : {0}kr", tillbaka);
+            Console.WriteLine("--------------------------");
+            return tillbaka;
+        }
+
+        private static double ReadUint(double attbetala, double avrundning)
+        {
+            double efteravrudning;
+
+            if (avrundning < 0.5)
+            {
+                efteravrudning = attbetala - avrundning;
+            }
+            else
+            {
+                efteravrudning = attbetala - avrundning + 1;
+            }
+            return efteravrudning;
+        }
+
+        private static void ReadPositiveDouble(out int betalamed, out int attbetala2, out double attbetala, out double avrundning)
+        {
 
 
 
-            Console.ReadKey();
+
+            Console.Write("Ange total summa    : ");
+            attbetala = double.Parse(Console.ReadLine());
+            Console.Write("Ange erhållet belopp: ");
 
 
 
-
-
-
-
-
-
-
-
-
-
+            betalamed = int.Parse(Console.ReadLine());
+            attbetala2 = (int)attbetala;
+            avrundning = (double)attbetala - attbetala2;
         }
     }
 }
