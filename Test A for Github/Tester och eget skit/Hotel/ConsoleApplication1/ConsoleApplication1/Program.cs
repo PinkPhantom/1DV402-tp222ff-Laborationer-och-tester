8f74097339c2ab8219956153e949fe36e9a5c953
Truls Persson <tp222ff@student.lnu.se>
2014-09-16 20:05:05 +0200

    En förbättring på Golf och lab 1.3 är ett försök och början till c nivå

---
+++ Test A for Github/Tester och eget skit/Hotel/ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int salaries;
+
+            salaries = 0;
+
+            do
+            {
+                while (salaries < 2)
+
+                {
+                salaries = ReadInt();
+
+                if (salaries < 2)
+                {
+                    Console.BackgroundColor = ConsoleColor.Red;
+
+                }
+                }
+
+            }
+
+        }
+    }
+}
